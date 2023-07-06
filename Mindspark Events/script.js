fb570e3e1cd42a546615843c9c0032c1724c854a
HimanshuKamdi <94886991+HimanshuKamdi@users.noreply.github.com>
2023-07-06 11:55:28 +0530

    Delete Mindspark Events directory

--- Mindspark Events/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-let animation=document.getElementById('animation');
-let height = document.body.scrollHeight - (window.innerHeight) ;
-animation.style.height = height + "px";
-
-
-});
